d71e5207d1e48ccc13897594237a7a5a38ec72a2
Cederic Bosmans <c.bosmans@napoleongames.be>
2020-08-17 18:05:51 +0200

    add Light

---
+++ demo.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Demo for the TP-Link A19-LB130 RBGW WiFi bulb
+'''
+
+
+import time
+from tplight import LB130
+
+def main():
+    '''
+    Main program function
+    '''
+
+    # create an instance of the light with its IP address
+    light = LB130("192.168.0.142")
+
+    # fetch the details for the light
+    print("Device ID: " + light.device_id)
+    print("Alias: " + light.alias)
+    print("Wattage: " + str(light.wattage))
+
+    light.off()
+    time.sleep(1)
+
+    light.on()
+    time.sleep(1)
+
+    brightness(light)
+    flashing_light(light)
+
+
+def flashing_light(light):
+    print("Flashing the light")
+    light.off()
+    time.sleep(2)
+
+    light.on()
+    time.sleep(2)
+
+    light.off()
+    time.sleep(2)
+
+    light.on()
+    time.sleep(2)
+
+    light.off()
+    time.sleep(2)
+
+    light.on()
+    time.sleep(2)
+
+    print("Done flashing")
+
+def brightness(light):
+    print("set brightness")
+    # set the brightness
+    light.brightness = 1
+    time.sleep(2)
+
+    light.brightness = 20
+    time.sleep(2)
+
+    light.brightness = 40
+    time.sleep(2)
+
+    light.brightness = 60
+    time.sleep(2)
+
+    light.brightness = 80
+    time.sleep(2)
+
+    light.brightness = 100
+    time.sleep(2)
+
+    print("brightness done")
+
+def saturation(light):
+    print("set saturation")
+    # set the saturation
+    light.saturation = 10
+    time.sleep(2)
+
+    # set the saturation
+    light.saturation = 30
+    time.sleep(2)
+
+    # set the saturation
+    light.saturation = 50
+    time.sleep(2)
+
+    # set the saturation
+    light.saturation = 70
+    time.sleep(2)
+
+    # set the saturation
+    light.saturation = 100
+    time.sleep(2)
+
+    print("saturation done")
+
+def temperature(light):
+    print("set temp")
+    light.temperature = 2500
+    time.sleep(2)
+
+    light.temperature = 9000
+    time.sleep(2)
+
+    light.temperature = 3800
+    time.sleep(2)
+
+    light.temperature = 5000
+    time.sleep(2)
+
+    print("temp done")
+
+if __name__ == "__main__":
+    main()
+
--- distance.py
+++ distance.py
@@ -3,7 +3,9 @@
 #Libraries
 import RPi.GPIO as GPIO
 import time
- 
+from tplight import LB130
+
+
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
  
@@ -43,10 +45,21 @@ def distance():
     return distance
  
 if __name__ == '__main__':
+
+    # create an instance of the light with its IP address
+    light = LB130("192.168.0.142")
+
     try:
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
+
+            if dist < 3:
+                light.off()
+
+            if dist > 3:
+                light.on()
+                      
             time.sleep(1)
  
         # Reset by pressing CTRL + C
